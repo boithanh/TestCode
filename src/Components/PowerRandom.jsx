0937ec049d67b8fcd318259031d3ed48d5bcc59a
boithanh <boithanh01694@gmail.com>
2025-04-02 23:07:29 +0700

    Sort result random numbers tt

--- src/Components/PowerRandom.jsx
+++ src/Components/PowerRandom.jsx
@@ -9,7 +9,7 @@ const PowerRandom = ({ animate, runningAnimate }) => {
             <img src="./../../lottery_3.png" className="card-img-top" alt="err" />
             <div className="card-body">
                 <h5 className="card-title">Random Power 6/55 👇</h5>
-                <p className="card-text p-4 border border-1 border-dark bold fw-bold">{power.join(", ").toString()}</p>
+                <p className="card-text p-4 border border-1 border-dark bold fw-bold">{power.join(" ").toString()}</p>
                 <button className='btn btn-primary mb-3' onClick={() => {
                     setPower(generateRandomNumbers(55));
                     runningAnimate(animate, 2);
